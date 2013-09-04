a69be7b2c16a494cc32c241e36578f4ddb058e2b
MLN <mlnkrish86@gmail.com>
2013-09-04 16:35:56 +0530

    rails prj with twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auto::Application.config.secret_token = '26b7265e379c7f14f5cb2f870d1101eda8688e9b06dd32498bdf6f936c1eb3b5c2adc137c23abc0187447204316ebccd9474a35dc4c09ce6cfc762b3080f4069'
